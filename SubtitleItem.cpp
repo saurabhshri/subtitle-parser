51f1fe44bb31bb3a808041bd080a5b60c36b64ea
Saurabh Shrivastava <saurabh.shrivastava54@gmail.com>
2017-05-15 15:45:55 +0530

    Extracting non-dialogues and speaker.
    Early commit; non-proof code.  Temperature is 45 degress outside and my
    laptop keyboard is burning. Tired of coding. Shall resume at night!

--- SubtitleItem.cpp
+++ SubtitleItem.cpp
@@ -3,6 +3,49 @@
 #include <regex>
 #include "SubtitleItem.h"
 
+/*
+#if (defined (WIN32) || defined (_WIN32_WCE)) && (defined(__MINGW32__) || !defined(__GNUC__))
+#define LLD_M "I64d"
+#define LLU_M "I64u"
+#define LLD "%I64d"
+#define LLU "%I64u"
+#elif defined (__SYMBIAN32__)
+#define LLD_M "d"
+#define LLU_M "u"
+#define LLD "%d"
+#define LLU "%u"
+#elif defined(__DARWIN__) || defined(__APPLE__)
+#define LLD_M "lld"
+#define LLU_M "llu"
+#define LLD "%lld"
+#define LLU "%llu"
+#elif defined(_LP64) // Unix 64 bits
+#define LLD_M "ld"
+#define LLU_M "lu"
+#define LLD "%ld"
+#define LLU "%lu"
+#else // Unix 32 bits
+#define LLD_M "lld"
+#define LLU_M "llu"
+#define LLD "%lld"
+#define LLU "%llu"
+#endif
+
+char* timestamp_to_srttime(unsigned long long milliseconds) {
+    unsigned long long millis = milliseconds % 1000;
+    milliseconds /= 1000;
+    unsigned long long seconds = milliseconds % 60;
+    milliseconds /= 60;
+    unsigned long long minutes = milliseconds % 60;
+    milliseconds /= 60;
+    unsigned long long hours = milliseconds;
+
+    char* buf = new char[(sizeof(char) * 15)];
+    sprintf(buf, "%02" LLD_M ":%02" LLD_M ":%02" LLD_M ",%03" LLD_M, hours, minutes, seconds, millis);
+    return buf;
+}
+*/
+
 SubtitleItem::SubtitleItem(void)
 {
 }
@@ -12,6 +55,14 @@ SubtitleItem::SubtitleItem(std::string startTime,std::string endTime, std::strin
 	_startTime = timeMSec(startTime);
 	_endTime = timeMSec(endTime);
 	_text = text;
+
+    _startTimeString = startTime;
+    _endTimeString = endTime;
+
+//    char *time = new char[1024];
+//    time = timestamp_to_srttime(_startTime);
+//
+//    std::cout<<startTime<<" ---> "<<time<<std::endl;
 }
 
 SubtitleItem::SubtitleItem(std::string startTime,std::string endTime, std::string text, bool ignore = false,
@@ -23,7 +74,9 @@ SubtitleItem::SubtitleItem(std::string startTime,std::string endTime, std::strin
 	_endTime = timeMSec(endTime);
 	_text = text;
 
-	_ignore = ignore;
+    _startTimeString = startTime;
+    _endTimeString = endTime;
+    _ignore = ignore;
 	_justDialogue = justDialogue;
 	_speakerCount = speakerCount;
 	_nonDialogueCount = nonDialogueCount;
@@ -78,6 +131,16 @@ SubtitleItem::~SubtitleItem(void)
 
 }
 
+std::string SubtitleItem::getStartTimeString() const
+{
+    return _startTimeString;
+}
+
+std::string SubtitleItem::getEndTimeString() const
+{
+    return _endTimeString;
+}
+
 bool SubtitleItem::getIgnoreStatus() const
 {
 	if(_ignore)
@@ -88,30 +151,86 @@ bool SubtitleItem::getIgnoreStatus() const
 
 }
 
-void SubtitleItem::extractInfo()
+void SubtitleItem::extractInfo(bool keepHTML = 0, bool doNotIgnoreNonDialogues = 0)
 {
-    //stripping HTML tags
-
-    std::regex tags("<[^<]*>");
     std::string output;
-    std::regex_replace(std::back_inserter(output), _text.begin(), _text.end(), tags, "");
+
+    //stripping HTML tags
+    if(!keepHTML)
+    {
+        std::regex tags("<[^<]*>");
+        std::regex_replace(std::back_inserter(output), _text.begin(), _text.end(), tags, "");
+    }
 
     //stripping non dialogue data e.g. (applause)
     //EXPERIMENTAL
-    std::regex brackets("([^(]*)");
-    std::regex_replace(std::back_inserter(output), output.begin(), output.end(), tags, "");
+    if(!doNotIgnoreNonDialogues)
+    {
+        int countP = 0;
+        for(char& c : output)
+        {
+            if(c=='(')
+            {
+                countP++;
+                c = '~';
+            }
 
-    _justDialogue = output;
+            else
+            {
+                if(countP!=0)
+                {
+                    if(c != ')')
+                        c = '~';
 
-    std::cout<<"**********************"<<_justDialogue<<std::endl;
+                    else if(c == ')')
+                    {
+                        c = '~';
+                        countP--;
+                    }
+                }
+            }
+        }
+
+        output.erase(std::remove(output.begin(), output.end(), '~'), output.end());
+
+        //Extracting speaker names
+
+        for(int i=0; output[i]!='\0';i++)
+        {
+            int colonIndex = 0, prevSpaceIndex = 0, length = 0;
+            if(output[i]==':')  //speaker found; travel back
+            {
+                _speakerCount++;
+                colonIndex = i;
+
+                for(int j=i; j>0;j--)
+                {
+                    if(output[j]== ' ' || output[j]== '\n' )
+                    {
+                        prevSpaceIndex = j;
+                        //i = prevSpaceIndex;
+                        break;
+                    }
+                }
+                
+
+                _speaker.push_back(output.substr(prevSpaceIndex+1, colonIndex-prevSpaceIndex-1));
+                output.erase(prevSpaceIndex+1, colonIndex-prevSpaceIndex);
+            }
+
+
+
+        }
+        std::cout<<output<<std::endl;
+    }
 
 
 }
 
-std::string SubtitleItem::getDialogue()
+std::string SubtitleItem::getDialogue(bool keepHTML = 0, bool doNotIgnoreNonDialogues = 0)
 {
 	if(_justDialogue.empty())
-		extractInfo();
+		extractInfo(keepHTML, doNotIgnoreNonDialogues);
 
 	return _justDialogue;
 }
